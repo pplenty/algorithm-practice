b01fed3d867d8317b519ff4b8efd68fcc521d2e2
pplenty <newtype26@naver.com>
2019-03-11 18:24:56 +0900

    CompletableFuture test

--- src/main/java/com/jason/java8/CompletableFutureTest.java
+++ src/main/java/com/jason/java8/CompletableFutureTest.java
@@ -1,6 +1,10 @@
 package com.jason.java8;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CompletableFutureTest {
 
@@ -16,9 +20,48 @@ public class CompletableFutureTest {
 
     public static void main(String[] args) throws Exception {
 
-        CompletableFutureTest main = new CompletableFutureTest();
-        main.completableFuture();
+//        CompletableFutureTest main = new CompletableFutureTest();
+//        main.completableFuture();
+//
+//        Future<String> future = main.calculateAsync();
+//
+//        while (!future.isDone()) {
+//            String result = future.get();
+//            System.out.println(result);
+//        }
 
+
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Hello";
+        });
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(500l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Beautiful";
+        });
+        CompletableFuture<String> future3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(100l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "World";
+        });
+
+        CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(future1, future2, future3);
+
+        String combined = Stream.of(future1, future2, future3).map(CompletableFuture::join)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(combined);
 
     }
 
@@ -33,5 +76,18 @@ public class CompletableFutureTest {
                     return null;
                 });
         Thread.sleep(11 * 1000L);
+    }
+
+    public Future<String> calculateAsync() throws InterruptedException {
+        CompletableFuture<String> completableFuture
+                = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(500);
+            completableFuture.complete("Hello");
+            return null;
+        });
+
+        return completableFuture;
     }
 }
