052e2b4a0e814d9b63c2373593e00b529ccf8b7b
pplenty <newtype26@naver.com>
2020-02-13 00:28:43 +0900

    study: Counting Elements

--- src/main/java/com/jason/site/codility/lesson4/PermCheck.java
+++ src/main/java/com/jason/site/codility/lesson4/PermCheck.java
@@ -1,7 +1,6 @@
 package com.jason.site.codility.lesson4;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * @author : kohyusik
@@ -16,28 +15,33 @@ public class PermCheck {
 
         // test case 1
         int[] test1 = new int[]{4, 1, 2, 3};
-        int count = permCheck.solution(test1);
-        System.out.println("Answer : " + count);
+        int result1 = permCheck.solution(test1);
+        System.out.println("Answer : " + result1);
+
+        // test case 2
+        int[] test2 = new int[]{4, 1, 3};
+        int result2 = permCheck.solution(test2);
+        System.out.println("Answer : " + result2);
+
+        // test case 3
+        int[] test3 = new int[]{Integer.MAX_VALUE};
+        int result3 = permCheck.solution(test3);
+        System.out.println("Answer : " + result3);
 
     }
 
     public int solution(int[] A) {
 
+        Arrays.sort(A);
 
-        List<Integer> cache = new ArrayList<>();
+        if (A[0] != 1 || A[A.length - 1] != A.length) {
+            return 0;
+        }
 
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] > A.length) {
+        for (int i = 1; i < A.length - 1; i++) {
+            if (A[i + 1] - A[i] != 1) {
                 return 0;
-            } else {
-
-                if (cache.indexOf(A[i]) != -1) {
-                    return 0;
-                }
-                cache.add(A[i]);
             }
-
-
         }
 
         return 1;
